46d9e15a676c15121cfb6c41cecb92b4f553af2e
Rafael Pol <rafael.pol1@upr.edu>
2013-10-05 00:37:16 -0400

    Added the following implementations
    Talking with server
    1)cart
    2)seller store
    3)user profile
    4)account info

--- Spruce/appjs/app.js
+++ Spruce/appjs/app.js
@@ -208,6 +208,112 @@ function ajaxMySpruce(where){
 	});
 }
 
+
+///////////////////////////////////////////////!!!!!!!!!!!!!!//////////////////////////
+/////////////////////////////////////////////////////NEWWWWWWWWWWWWWWW////////////////////////////////////////////
+$(document).on('pagebeforeshow', "#cartView", function( event, ui ) {
+	console.log("Jose");
+	$.ajax({
+		url : "http://localhost:3412/SpruceTestServer/user/cart",
+		contentType: "application/json",
+		success : function(data, textStatus, jqXHR){
+			var objectList = data.cart;
+			var len = objectList.length;
+			var list = $("#myCartList");
+			list.empty();
+			var total=0;
+			var object;
+			for (var i=0; i < len; ++i){
+				object = objectList[i];
+				list.append('<li data-icon="false"><a href="buyerproduct.html"><img src="images/itshappening.gif" style="resize:both; overflow:scroll; width:80px; height:80px">'+
+				'<div class="ui-grid-a"><div class="ui-block-a"><h1 style="margin: 0px">'+object.name+'</h1><p style="font-size: 13px;margin-top:0px"><b>'+object.model+'</b></p><p>'+object.brand+'</p>'+
+				'</div><div class="ui-block-b" align="right"><h1 style="font-size: 16px" >'+accounting.formatMoney(object.price)+'</h1><p>Amount:'+object.amount+'</p></div></div><a href="#dialogPage"  data-rel="dialog"></a>');
+				total+=object.price*object.amount;
+			}
+			list.listview("refresh");	
+			$("#checkoutButton .ui-btn-text").text("Checkout ("+accounting.formatMoney(total)+")");
+		},
+		error: function(data, textStatus, jqXHR){
+			console.log("textStatus: " + textStatus);
+			alert("Data not found!");
+		}
+	});
+});
+
+//////////////////////////SELLER STORE//////////////////////////////////
+$(document).on('pagebeforeshow', "#userStore", function( event, ui ) {
+	console.log("Jose");
+	$.ajax({
+		url : "http://localhost:3412/SpruceTestServer/user/store",
+		contentType: "application/json",
+		success : function(data, textStatus, jqXHR){
+			var objectList = data.items;
+			var len = objectList.length;
+			var list = $("#sellerItems");
+			list.empty();
+			var object;
+			for (var i=0; i < len; ++i){
+				object = objectList[i];
+				list.append('<li data-icon="false"><a  onclick="GetItem('+object.id+')"><img style="padding-left:5px; padding-top: 7px"src="css/images/thumbnail.png">'+ 
+			'<h1 style="margin: 0px">'+object.name+'</h1><hr style="margin-bottom: 0px;margin-top: 3px"/><div class="ui-grid-a"><div class="ui-block-a" align="left" style="">'+
+			'<h2 style="font-size: 13px;margin-top:0px">'+object.model+'</h2><p>'+object.brand+'</p></div><div class="ui-block-b" align="right">'+
+			'<h3 style="margin-top:0px;padding-top: 0px">'+accounting.formatMoney(object.price)+'</h3><p><b>'+object.startingDate+'</b></p></div></div></a></li>');
+				
+			}
+			list.listview("refresh");	
+		},
+		error: function(data, textStatus, jqXHR){
+			console.log("textStatus: " + textStatus);
+			alert("Data not found!");
+		}
+	});
+});
+
+
+////////////////////////////////////USER PROFILE////////////////////////////////////////
+$(document).on('pagebeforeshow', "#userprofile", function( event, ui ) {
+	$.mobile.loading("show");
+	$.ajax({
+		url : "http://localhost:3412/SpruceTestServer/user/profile",
+		contentType: "application/json",
+		success : function(data, textStatus, jqXHR){
+			var currentUser = data.user;
+			$('#userProfileName').text(currentUser.userName); 
+			$('#userProfileEmail').text(currentUser.email); 
+			//$("#userProfileImage").attr("src","./images/"+currentUser.name+".jpg");
+			$('#addressProfileName').html(currentUser.address+"</br>"+currentUser.city+", "+currentUser.state+" "+currentUser.zip); 
+			$('#userProfilePhone').text(currentUser.phone);
+			$.mobile.loading("hide");
+		},
+		error: function(data, textStatus, jqXHR){
+			console.log("textStatus: " + textStatus);
+			alert("Data not found!");
+		}
+	});
+});
+
+/////////////////////////ACCOUNT INFO/////////////////////////
+$(document).on('pagebeforeshow', "#accountinfo", function( event, ui ) {
+	$.mobile.loading("show");
+	$.ajax({
+		url : "http://localhost:3412/SpruceTestServer/user/profile",
+		contentType: "application/json",
+		success : function(data, textStatus, jqXHR){
+			var currentUser = data.user;
+			$('#userMyAccountName').text(currentUser.userName); 
+			$('#userMyAccountEmail').text(currentUser.email); 
+			//$("#userProfileImage").attr("src","./images/"+currentUser.name+".jpg");
+			$('#addressMyAccountName').html(currentUser.address+"</br>"+currentUser.city+", "+currentUser.state+" "+currentUser.zip); 
+			$('#userMyAccountPhone').text(currentUser.phone);
+			$.mobile.loading("hide");
+		},
+		error: function(data, textStatus, jqXHR){
+			console.log("textStatus: " + textStatus);
+			alert("Data not found!");
+		}
+	});
+});
+
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /// Functions Called Directly from Buttons ///////////////////////
 
