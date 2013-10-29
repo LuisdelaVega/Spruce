ea026cdaa9b40721ac1d231195c8bbdd59399fa6
Luis R. de la Vega López <luis.delavega@upr.edu>
2013-10-29 18:56:45 -0400

    I made examples of this. We just need to implement this in every view

--- Spruce/appjs/application.js
+++ Spruce/appjs/application.js
@@ -63,6 +63,29 @@ $(document).on('pagebeforeshow', "#lrd-home", function(event, ui) {
 
 $(document).on('pagebeforeshow', "#lrd-itemsforcategory", function( event, ui ) {
 	$.ajax({
+		url : "http://localhost:3412/SpruceServer/getCategoriesForSidePanel",
+		method: 'get',
+		contentType: "application/json",
+		success : function(data, textStatus, jqXHR){
+			var objectList = data.categories;
+			var len = objectList.length;
+			var list = $("#lrd-itemsforcategorySidePanel");
+			list.empty();
+			var object;
+			list.append("<li><a onclick=GoToView('lrd-home')>Home</a></li><li><a  onclick=GoToView('lrd-myspruce') >My Spruce</a></li><li data-role=list-divider data-theme=a>Categories</li>");
+			for (var i=0; i < len; ++i){
+				object = objectList[i];
+				list.append("<li><a onclick=GetItemsForCategory("+object.catid+")>"+object.catname+"</a></li>");
+			}
+			list.append("<li data-role=list-divider data-theme=a></li><li><a onclick=GoToView('lrd-myaccountinfo') >My Account Info</a></li><li><a onclick=GoToView('lrd-adminreportspage') >Admin Tools</a></li><li><a href=#candy >Sign Out</a></li>");
+			list.listview("refresh");	
+		},
+		error: function(data, textStatus, jqXHR){
+			console.log("textStatus: " + textStatus);
+			alert("Data not found!");
+		}
+	});
+	$.ajax({
 		url : "http://localhost:3412/SpruceServer/getItemsForCategory/"+sessionStorage.category,
 		method: 'get',
 		contentType: "application/json",
@@ -82,7 +105,7 @@ $(document).on('pagebeforeshow', "#lrd-itemsforcategory", function( event, ui ) 
 					'<h3 style="margin-top:0px;padding-top: 0px">'+accounting.formatMoney(object.price)+'</h3><p><b>'+object.itemdate+'</b></p></div></div></a></li>');
 				
 			}
-			list.listview("refresh");	
+			list.listview("refresh");
 		},
 		error: function(data, textStatus, jqXHR){
 			console.log("textStatus: " + textStatus);
@@ -92,15 +115,14 @@ $(document).on('pagebeforeshow', "#lrd-itemsforcategory", function( event, ui ) 
 });
 
 $(document).on('pagebeforeshow', "#lrd-login", function( event, ui ) {
-	$.ajax({
+	  $.ajax({
 		url : "http://localhost:3412/SpruceServer/getCategoriesForSidePanel",
 		method: 'get',
 		contentType: "application/json",
 		success : function(data, textStatus, jqXHR){
-			
 			var objectList = data.categories;
 			var len = objectList.length;
-			var list = $("#sidePanel");
+			var list = $("#lrd-loginSidePanel");
 			list.empty();
 			var object;
 			list.append("<li><a onclick=GoToView('lrd-home')>Home</a></li><li><a  onclick=GoToView('lrd-myspruce') >My Spruce</a></li><li data-role=list-divider data-theme=a>Categories</li>");
@@ -235,6 +257,29 @@ $(document).on('pagebeforeshow', "#lrd-bidhistory", function( event, ui ) {
 });
 
 $(document).on('pagebeforeshow', "#lrd-cart", function( event, ui ) {
+	$.ajax({
+		url : "http://localhost:3412/SpruceServer/getCategoriesForSidePanel",
+		method: 'get',
+		contentType: "application/json",
+		success : function(data, textStatus, jqXHR){
+			var objectList = data.categories;
+			var len = objectList.length;
+			var list = $("#lrd-cartSidePanel");
+			list.empty();
+			var object;
+			list.append("<li><a onclick=GoToView('lrd-home')>Home</a></li><li><a  onclick=GoToView('lrd-myspruce') >My Spruce</a></li><li data-role=list-divider data-theme=a>Categories</li>");
+			for (var i=0; i < len; ++i){
+				object = objectList[i];
+				list.append("<li><a onclick=GetItemsForCategory("+object.catid+")>"+object.catname+"</a></li>");
+			}
+			list.append("<li data-role=list-divider data-theme=a></li><li><a onclick=GoToView('lrd-myaccountinfo') >My Account Info</a></li><li><a onclick=GoToView('lrd-adminreportspage') >Admin Tools</a></li><li><a href=#candy >Sign Out</a></li>");
+			list.listview("refresh");	
+		},
+		error: function(data, textStatus, jqXHR){
+			console.log("textStatus: " + textStatus);
+			alert("Data not found!");
+		}
+	});
 	$.ajax({
 		url : "http://localhost:3412/SpruceServer/user/cart",
 		contentType: "application/json",
